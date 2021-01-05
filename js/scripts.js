00c338d606fc2026a72f88ea4a46010073a2d7e9
thebadcc <thebadcc@outlook.com>
2021-01-05 10:31:39 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
- // tabbed content
-    // http://www.entheosweb.com/tutorials/css/tabs.asp
+
     $(".tab_content").hide();
     $(".tab_content:first").show();
 
   /* if in tab mode */
-    $("ul.tabs li").click(function() {
+    $("ul.tabsx li").click(function() {
 		
       $(".tab_content").hide();
       var activeTab = $(this).attr("rel"); 
       $("#"+activeTab).fadeIn();		
 		
-      $("ul.tabs li").removeClass("active");
+      $("ul.tabsx li").removeClass("active");
       $(this).addClass("active");
 
 	  $(".tab_drawer_heading").removeClass("d_active");
@@ -27,12 +26,8 @@
 	  $(".tab_drawer_heading").removeClass("d_active");
       $(this).addClass("d_active");
 	  
-	  $("ul.tabs li").removeClass("active");
-	  $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+	  $("ul.tabsx li").removeClass("active");
+	  $("ul.tabsx li[rel^='"+d_activeTab+"']").addClass("active");
     });
 	
-	
-	/* Extra class "tab_last" 
-	   to add border to right side
-	   of last tab */
-	$('ul.tabs li').last().addClass("tab_last");
+	$('ul.tabsx li').last().addClass("tab_last");
