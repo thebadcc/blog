a1366de67a62f9fd6f6878b90dd9b8057b2c411d
thebadcc <thebadcc@outlook.com>
2021-01-09 15:01:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,27 @@
 })(jQuery);
 
 
+(function($){
+ $(function(){
+  $('nav ul li > a:not(:only-child)').click(function(e) {
+    $(this).siblings('.nav-dropdown').toggle();
+    $('.nav-dropdown').not($(this).siblings()).hide();
+    e.stopPropagation();
+  });
+ });
+ $('html').click(function() {
+  $('.nav-dropdown').hide();
+ });
+ document.querySelector('#nav-toggle').addEventListener('click', function() {
+  this.classList.toggle('active');
+ });
+ $('#nav-toggle').click(function() {
+  $('nav ul').toggle();
+ });
+})(jQuery);
+
+
+
 $(".tab_content").hide();
     $(".tab_content:first").show();
     $("ul.nav-dropdown li").click(function() {
