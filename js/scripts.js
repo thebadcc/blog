5f65125d6fae06e18badd8b582561f24be128165
thebadcc <thebadcc@outlook.com>
2021-01-09 15:06:15 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,20 +19,10 @@
 
 
 (function($){
- $(function(){
-  $('nav ul li > a:not(:only-child)').click(function(e) {
-    $(this).siblings('.nav-dropdown').toggle();
-    $('.nav-dropdown').not($(this).siblings()).hide();
-    e.stopPropagation();
-  });
- });
- $('html').click(function() {
-  $('.nav-dropdown').hide();
- });
- document.querySelector('#nav-toggle').addEventListener('click', function() {
+ document.querySelector('#back-toggle').addEventListener('click', function() {
   this.classList.toggle('active');
  });
- $('#nav-toggle').click(function() {
+ $('#back-toggle').click(function() {
   $('nav ul').toggle();
  });
 })(jQuery);
