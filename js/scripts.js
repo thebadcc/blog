5ad309dc022f633aaf6e3c2769216170613a37aa
thebadcc <thebadcc@outlook.com>
2021-02-14 22:43:24 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -134,4 +134,16 @@ $(document).ready(function() {
     })  
 } );
 
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
 
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
