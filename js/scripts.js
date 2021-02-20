f6b16ee80d85a9bc426b68a24164a7923e189fa6
thebadcc <thebadcc@outlook.com>
2021-02-20 18:49:13 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -174,6 +174,9 @@ ethereumButton.addEventListener('click', () => {
 
 async function getAccount() {
   accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  showAccount.innerHTML = accounts;
+    var front = accounts.substring(0, 5);
+	showAccount.innerHTML = front;
+
+
   connectAccount.innerHTML = "";
 }
