fadc81b45087400ab39f143e8476bdba0b40ac55
thebadcc <thebadcc@outlook.com>
2021-02-20 22:29:30 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -186,3 +186,28 @@ async function getAccount() {
 
   connectAccount.innerHTML = "";
 }
+
+
+Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', function (data) {
+    // Create the chart
+    Highcharts.stockChart('container', {
+
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'AAPL Stock Price'
+        },
+
+        series: [{
+            name: 'AAPL',
+            data: data,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+});
+
