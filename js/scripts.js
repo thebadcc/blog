5a5ae3f27c789030cda2d5d783dbc20135a2e457
thebadcc <thebadcc@outlook.com>
2021-04-13 22:49:35 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -201,20 +201,3 @@ async function getAccount() {
     search.style = "display: block !important";
 }
 
-$(document).ready(function() {
-  var counter = 0;
-  var c = 0;
-  var i = setInterval(function(){
-      $(".loading-page .counter h1").html(c + "%");
-      $(".loading-page .counter hr").css("width", c + "%");
-    counter++;
-    c++;
-    if(counter == 101) {
-        clearInterval(i);
-        $('.loading-page').fadeOut();
-        $('.wrap').show();
-    }
-  }, 10);
-  
-});
-
