995427aba854d87b5e399d18510f602c95e31424
thebadcc <thebadcc@outlook.com>
2021-04-13 23:10:11 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -201,3 +201,27 @@ async function getAccount() {
     search.style = "display: block !important";
 }
 
+
+$(document).ready(function() {
+  
+  var counter = 0;
+  var c = 0;
+  var i = setInterval(function(){
+      $(".loading-page .counter h1").html(c + "%");
+      $(".loading-page .counter hr").css("width", c + "%");
+      //$(".loading-page .counter").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
+
+    /*
+    $(".loading-page .counter h1.color").css("width", c + "%");
+    */
+    counter++;
+    c++;
+      
+    if(counter == 101) {
+        clearInterval(i);
+        $('.loading-page').fadeOut();
+	$(body).show();
+	    
+    }
+  }, 50);
+});
