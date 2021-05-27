10a46f80d689a44e9a871ee796af535a9cacc868
thebadcc <thebadcc@outlook.com>
2021-05-27 19:56:02 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -278,3 +278,25 @@ $('#search-bar').keyup(function(){
        }
     });
 });
+
+
+const sendEthButton = document.querySelector('.executeTx');
+
+//Sending Ethereum to an address
+sendEthButton.addEventListener('click', () => {
+  ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: accounts[0],
+          to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+          value: '0x29a2241af62c0000',
+          gasPrice: '0x09184e72a000',
+          gas: '0x2710',
+        },
+      ],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+});
