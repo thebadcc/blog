eae0a0f6d648001d115ea45f94a5f2c49425981b
thebadcc <thebadcc@outlook.com>
2021-05-27 22:22:41 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -288,7 +288,7 @@ const provider = ethers.getDefaultProvider();
 const erc20 = new ethers.Contract(address, tester, provider);
 
 const symbol = erc20.symbol();
-console.log(symbol);
+console.log(symbol.Promise);
 
 const inputOne = document.getElementById('inputOne');
 const inputTwo = document.getElementById('inputTwo');
