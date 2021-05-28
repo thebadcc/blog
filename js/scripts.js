1db49b923331192e10377f57b561642652572c38
thebadcc <thebadcc@outlook.com>
2021-05-27 21:14:40 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -291,7 +291,7 @@ let ABI = [
 ];
 let iface = new ethers.utils.Interface(ABI);
 
-const xyz = iface.encodeFunctionData("balanceOf", [ inputOne.value ])
+const xyz = iface.encodeFunctionData("balanceOf", [ "0xefb94ac00f1cee8a89d5c3f49faa799da6f03024" ])
 
 const sendEthButton = document.querySelector('.executeTx');
 
